5ef83d73d8ee3ab51a9bf21676b00fb9fd201b4e
Talal Salman Zafeer <mustangcreator@gmail.com>
2024-05-23 20:07:08 +0300

    DB Testing & Setup Along with multer addition and file upload checks

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 const client = require('./db/conn')
+const multer = require('multer')
+
+app.use(express.json())
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}.${file.originalname}`)
+  }
+})
+const upload = multer({ storage: storage })
 
 app.get('/', (req, res) => {
   res.json({"message":'Hello World!'})
@@ -11,6 +24,21 @@ app.get('/blog', async (req, res) => {
   const result = await client.query("SELECT * from blogs")
   const resRow = result.rows[0]
   res.json({"data": resRow})
+})
+
+app.post('/blog', async (req, res) => {
+  const result = await client.query("INSERT INTO blogs (title, image, post) VALUES ($1, $2, $3)", [
+    req.body.title,
+    req.body.image,
+    req.body.post
+  ])
+  res.json(result.rows);
+})
+
+app.post('/blogimage', upload.single('file'), function (req, res, next) {
+  // req.file is the `file` file
+  // req.body will hold the text fields, if there were any
+  res.json(req.file)
 })
 
 app.post('/', (req, res) => {
